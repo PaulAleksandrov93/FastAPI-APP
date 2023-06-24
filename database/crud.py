ff30ddc3cd8e9eb957fcacd528514625548aa193
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-06-24 21:29:49 +0500

    first commit

---
+++ database/crud.py
@@ -0,0 +1,7 @@
+# import datetime
+# import pydantic_models
+# import models
+import bit
+
+wallet = bit.Key()  # наш кошелек готов и содержится в переменной wallet 
+print(wallet.get_balance())
---
+++ database/db.py
@@ -0,0 +1,4 @@
+from models import *
+
+# db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+db.bind(provider='sqlite', filename='C:\PythonProjects\TestBtc\database\database.sqlite', create_db=True)
